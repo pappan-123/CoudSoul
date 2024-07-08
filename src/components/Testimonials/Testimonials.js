3ae9eeab0e02bc77cf85a7800e77bd4a0f4baebd
pappan-123 <85434109+pappan-123@users.noreply.github.com>
2024-07-08 15:30:19 +0530

    Testimonials Texts updated

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -9,33 +9,27 @@ class Testimonials extends Component {
     super();
     this.state = {
       slidesPerView: 2.5,
-      reviews: [ // Expanded array of reviews
-        {
-          img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/3acce801-3fea-4bb9-bdaa-6229d2f714be_oqjhb8.jpg',
-          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.',
-          name: 'Tanvinder Singh',
-          occupation: 'Cloud Expert'
-        },
-        {
-          img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/37ddc39a-7691-48e4-ae7e-456910c77817_clgrh1.jpg',
-          comment: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.',
-          name: 'Angat Singh',
-          occupation: 'Internal Firm Affair'
-        },
-        {
-          img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/2f7b8a02-b894-4c0f-a8a4-d74e62e85ce3_yvknal.jpg',
-          comment: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-          name: 'Trivinder Singh',
-          occupation: 'Market Expert'
-        },
-        // {
-        //   img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/3e7e7f97-c5aa-4084-9cc8-faff944a8927_vv9jii.jpg',
-        //   comment: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est.',
-        //   name: 'Tandeep Singh',
-        //   occupation: 'Software Expert'
-        // }
-        // You can add more reviews as needed
-      ]
+reviews: [
+  {
+    img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/3acce801-3fea-4bb9-bdaa-6229d2f714be_oqjhb8.jpg',
+    comment: 'Our commitment to leveraging cutting-edge cloud solutions has significantly enhanced our operational efficiency and reduced costs, offering our clients unmatched value.',
+    name: 'Tanvinder Singh',
+    occupation: 'Cloud Expert'
+  },
+  {
+    img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/37ddc39a-7691-48e4-ae7e-456910c77817_clgrh1.jpg',
+    comment: 'Through strategic internal affairs management, we have streamlined processes and improved the transparency of operations, boosting productivity and client satisfaction',
+    name: 'Angat Singh',
+    occupation: 'Internal Firm Affairs Expert'
+  },
+  {
+    img: 'https://res.cloudinary.com/dxllpuliy/image/upload/v1713873541/2f7b8a02-b894-4c0f-a8a4-d74e62e85ce3_yvknal.jpg',
+    comment: 'As a market expert, my goal is to provide insightful strategies that drive market growth and innovation, leading to sustained business success and industry leadership.',
+    name: 'Trivinder Singh',
+    occupation: 'Market Expert'
+  }
+]
+
     };
     this.fitSlidesPerView = this.fitSlidesPerView.bind(this);
   }
